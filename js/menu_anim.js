d537090afd1b0e4fb1742f42ee29c15c995c9479
mspereira <miguel.silva.p@hotmail.com>
2015-05-26 14:18:20 +0100

    menu with section highlight

--- js/menu_anim.js
+++ js/menu_anim.js
@@ -22,4 +22,28 @@ $(window).scroll(function(){
             },600);
         }  
     }
+})
+$(document).ready(function(){
+    
+    $('.scroll').click(function(event) {
+        event.preventDefault();
+ 
+        var full_url = this.href;
+        
+        var parts = full_url.split('#');
+        var trgt = parts[1];
+
+        var target_offset = $('#'+trgt).offset();
+        var target_top = target_offset.top;
+
+        $('html, body').animate({scrollTop:target_top}, 500);
+
+
+    });
+    
+    $('nav#main a').click(function(){
+         $('nav#main a').removeClass('active');
+         $(this).addClass('active');
+    });
+    
 });
